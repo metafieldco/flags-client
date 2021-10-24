afe94d80f6b1484ec20913389e6b281f8fdcba2f
Archie <44258193+a-edwaar@users.noreply.github.com>
2021-10-24 14:43:53 +0100

    Initial Commit

---
+++ AsyncRecorder/AsyncRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncRecorderApp.swift
+//  AsyncRecorder
+//
+//  Created by Archie Edwards on 24/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
