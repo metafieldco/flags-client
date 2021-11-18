6b12891931bafa6d90fd0be5c17f26668bb89fbc
Archie <44258193+a-edwaar@users.noreply.github.com>
2021-11-18 09:43:08 +0000

    multi-device design experiment

---
+++ AsyncRecorder/Menu/ControlMenu.swift
@@ -0,0 +1,8 @@
+//
+//  ControlMenu.swift
+//  AsyncRecorder
+//
+//  Created by Archie Edwards on 17/11/2021.
+//
+
+import Foundation
---
+++ AsyncRecorder/Windows/Popup/PopoverView.swift
@@ -0,0 +1,20 @@
+//
+//  PopoverView.swift
+//  AsyncRecorder
+//
+//  Created by Archie Edwards on 17/11/2021.
+//
+
+import SwiftUI
+
+struct PopoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopoverView()
+    }
+}
